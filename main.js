67ca9c6675e9150300e8aef85931994ccd553af6
Quadcast <132080585+Quadcast@users.noreply.github.com>
2023-04-28 18:05:46 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/image.png') {
+      myImage.setAttribute ('src','images/icon2.png');
+    } else {
+      myImage.setAttribute ('src','images/image.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+    var myName = prompt('Write username.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Hello, ' + myName;
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Hello, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
+  
+  
+  
+
